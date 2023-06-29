778f0bad5bb701771825a6fdba77885f74b17379
NumberGuan <tomiortellado1@gmail.com>
2023-06-29 01:12:53 -0300

    Update visual al register y login

--- public/script.js
+++ public/script.js
@@ -1,18 +1,40 @@
 function checkAvailability(inputField) {
   const errorMessage = document.getElementById('error-message');
-  const existingUsers = ["user1", "user2", "user3"];
-
   const value = inputField.value;
   const validChars = /^[A-Za-z0-9]+$/;
+  const availableBorderColor = 'green';
+  const unavailableBorderColor = 'red';
 
-  if (inputField.name === 'usuario' && existingUsers.includes(value)) {
-    inputField.classList.add('error');
-    errorMessage.textContent = `El ${inputField.name} ya está en uso`;
-  } else if (inputField.name === 'usuario' && (value.length > 30 || !validChars.test(value) || value.includes('ñ'))) {
-    inputField.classList.add('error');
-    errorMessage.textContent = 'Caracter inválido. Solo se permiten letras y números.';
-  } else {
-    inputField.classList.remove('error');
-    errorMessage.textContent = '';
-  }
+  fetch('usuarios.json')
+    .then(response => response.json())
+    .then(existingUsers => {
+      if (inputField.name === 'usuario') {
+        if (existingUsers.some(user => user.usuario === value)) {
+          inputField.style.borderColor = unavailableBorderColor;
+          errorMessage.textContent = 'Nombre de usuario no disponible.';
+          errorMessage.classList.add('error-text');
+        } else if (value.length > 30 || !validChars.test(value) || value.includes('ñ')) {
+          inputField.style.borderColor = unavailableBorderColor;
+          errorMessage.textContent = value.length > 30 ? 'Limite de caracteres.' : 'Caracter inválido. Solo se permiten letras y números.';
+          errorMessage.classList.add('error-text');
+        } else {
+          inputField.style.borderColor = availableBorderColor;
+          errorMessage.textContent = '';
+          errorMessage.classList.remove('error-text');
+        }
+      } else if (inputField.name === 'correo') {
+        if (existingUsers.some(user => user.correo === value)) {
+          inputField.style.borderColor = unavailableBorderColor;
+          errorMessage.textContent = 'Correo electrónico no disponible.';
+          errorMessage.classList.add('error-text');
+        } else {
+          inputField.style.borderColor = availableBorderColor;
+          errorMessage.textContent = '';
+          errorMessage.classList.remove('error-text');
+        }
+      }
+    })
+    .catch(error => {
+      console.error('Error al cargar los usuarios:', error);
+    });
 }
